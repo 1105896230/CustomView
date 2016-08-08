82466f8e151d7c6ab1a2774b33ed7024b9f05476
fmt <1105896230@qq.com>
2016-08-08 22:32:06 +0800

    加入java8联系

---
+++ java8demo/src/main/java/com/example/java8demo/ApplePredicate.java
@@ -0,0 +1,11 @@
+package com.example.java8demo;
+
+/**
+ * Created by "林其望".
+ * DATE: 2016:08:08:22:25
+ * email:1105896230@qq.com
+ */
+
+public interface ApplePredicate {
+    public boolean test(Apple a);
+}
---
+++ java8demo/src/main/java/com/example/java8demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.java8demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       ApplePredicate applePredicate=(Apple apple)-> "red".equals(apple.getColor());
+        
+    }
+}

4263bbdec4756a573aa56819dd98a46fc2257c58
fmt <1105896230@qq.com>
2016-08-08 23:08:40 +0800

    lamdba

--- java8demo/src/main/java/com/example/java8demo/MainActivity.java
+++ java8demo/src/main/java/com/example/java8demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.java8demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.*;
 
@@ -11,7 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       ApplePredicate applePredicate=(Apple apple)-> "red".equals(apple.getColor());
-        
+        ApplePredicate applePredicate = (Apple apple) -> "red".equals(apple.getColor());
+
+
+//        ()->42
+//        (Apple e)->e.getWeight()>50
+//        (String s)->s.length()
+//        (int x, int y) -> {
+//            Log.e("test", x + "");
+//            Log.e("test", y + "");
+//        }
     }
 }

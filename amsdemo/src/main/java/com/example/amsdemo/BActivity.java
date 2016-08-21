1898de25cd9553bbbea604b853c9c63e590be6b3
fmt <1105896230@qq.com>
2016-08-21 20:39:29 +0800

    FLAG_ACTIVITY_FORWARD_RESULT

--- amsdemo/src/main/java/com/example/amsdemo/BActivity.java
+++ amsdemo/src/main/java/com/example/amsdemo/BActivity.java
@@ -2,31 +2,34 @@ package com.example.amsdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class BActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_b);
+
     }
+
 
 
     public void click(View view) {
-        Intent intent = new Intent(this, BActivity.class);
-        startActivityForResult(intent, 200);
+        Intent intent = new Intent(this, CActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+        startActivity(intent);
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (data != null) {
-            Log.e("main", "!=null");
+            Log.e("BActivity", "!=null");
         } else {
-            Log.e("main", "==null");
+            Log.e("BActivity", "==null");
         }
     }
 }

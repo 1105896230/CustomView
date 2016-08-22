3c2abf4cfb14b7f3f6a8cbec005cf7950eaa589c
fmt <1105896230@qq.com>
2016-08-22 19:50:51 +0800

    例子暴走

--- app/src/main/java/custom/fmt/com/mycustomview/MainActivity.java
+++ app/src/main/java/custom/fmt/com/mycustomview/MainActivity.java
@@ -3,34 +3,54 @@ package custom.fmt.com.mycustomview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.view.ViewGroup;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-
+import custom.fmt.com.mycustomview.views.ExplosionField.ExplosionField;
 import custom.fmt.com.mycustomview.zhujie.ContentView;
-import custom.fmt.com.mycustomview.zhujie.OnClick;
-import custom.fmt.com.mycustomview.zhujie.ViewInject;
 import custom.fmt.com.mycustomview.zhujie.ViewInjectUtils;
 
 @ContentView(value = R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-    @ViewInject(R.id.iv)
-    private ImageView imageView;
+
+    private ExplosionField mExplosionField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        ViewInjectUtils.inject(this);
+        ViewInjectUtils.inject(this);
+        mExplosionField = ExplosionField.attach2Window(this);
+        addListener(findViewById(R.id.root));
     }
 
-    @OnClick({R.id.iv})
-    public void OnBindCLick(View view) {
-        switch (view.getId()) {
-            case R.id.iv:
-                Toast.makeText(MainActivity.this, "注解成功", Toast.LENGTH_SHORT).show();
-                break;
+    private void addListener(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                addListener(parent.getChildAt(i));
+            }
+        } else {
+            root.setClickable(true);
+            root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mExplosionField.explode(v);
+                    v.setOnClickListener(null);
+                }
+            });
         }
     }
+
+    private void reset(View root) {
+        if (root instanceof ViewGroup) {
+            ViewGroup parent = (ViewGroup) root;
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                reset(parent.getChildAt(i));
+            }
+        } else {
+            root.setScaleX(1);
+            root.setScaleY(1);
+            root.setAlpha(1);
+        }
+    }
+
 }

23e543a5ccb95c52cd14ed63d0ea0c45e3a114c0
fmt <1105896230@qq.com>
2016-08-23 13:35:17 +0800

    加入aop

--- aopdemo/src/main/java/com/example/aopdemo/MainActivity.java
+++ aopdemo/src/main/java/com/example/aopdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.aopdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.aoplibrary.DebugTrace;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testAnnotatedMethod();
+    }
+
+    @DebugTrace
+    private void testAnnotatedMethod() {
+        try {
+            Thread.sleep(10);
+            Log.e("main","haha");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }

6f66e9ea864eae1a164db17ecc5583860fe6a778
fmt <1105896230@qq.com>
2016-08-28 21:35:43 +0800

    本地广播管理者

--- ndkproject/src/main/java/com/example/ndkproject/InhouseActivity.java
+++ ndkproject/src/main/java/com/example/ndkproject/InhouseActivity.java
@@ -1,11 +1,13 @@
 package com.example.ndkproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class InhouseActivity extends AppCompatActivity {
 
-    private static final String MY_PERMISSION="com.example.ndkproject.MY_PERSSION";
+    private static final String MY_PERMISSION = "com.example.ndkproject.MY_PERSSION";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +15,24 @@ public class InhouseActivity extends AppCompatActivity {
 //        if (!MY_PERMISSION.equals(getPermission(this))){
 //            finish();
 //        }
+//        LocalBroadcastManager.getInstance(this).sendBrocast();//发送异步广播
+//        LocalBroadcastManager.getInstance(this).sendBrocastSync();//发送同步广播
+//        LocalBroadcastManager.getInstance(this).registerReceiver();//注册广播
+//        LocalBroadcastManager.getInstance(this).unregisterReceiver();//注销广播
     }
+
+    private void senNormalBrocast() {
+        Intent intent = new Intent(this, PrivateBrocast.class);
+        intent.putExtra("param", "xxx");
+        sendBroadcast(intent);
+    }
+
+    private void senOrderBrocast() {
+        Intent intent = new Intent(this, PrivateBrocast.class);
+        intent.putExtra("param", "xxx");
+//        senOrderBrocast(intent, null, mRerever, null, 0, null);
+    }
+
+
+    PrivateBrocast mRerever;
 }
---
+++ ndkproject/src/main/java/com/example/ndkproject/LocalBrocast.java
@@ -0,0 +1,11 @@
+package com.example.ndkproject;
+
+/**
+ * Created by "林其望".
+ * DATE: 2016:08:28:21:18
+ * email:1105896230@qq.com
+ */
+
+public class LocalBrocast {
+
+}

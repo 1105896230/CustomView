acfa60732eb66f8916efb04ddb7a398c51d6e14e
fmt <1105896230@qq.com>
2016-08-28 20:48:20 +0800

    activity编码安全

---
+++ ndkproject/src/main/java/com/example/ndkproject/PublicActivity.java
@@ -0,0 +1,38 @@
+package com.example.ndkproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PublicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        //判断param来源的正确性，发送之前，可以适当做一些加密处理
+        String param = getIntent().getStringExtra("PARAM");
+    }
+
+    void onCliek(View view) {
+        Intent intent=new Intent();
+        intent.putExtra("RESULT","Not Sensitivie Info");
+        setResult(200,intent);
+    }
+
+}

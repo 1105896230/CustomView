8a59c47d538b7b06aba07d1106c00a8667428e98
fmt <1105896230@qq.com>
2016-09-05 14:29:14 +0800

    CreditSesameRingView

---
+++ creditsesameringview/src/main/java/com/example/creditsesameringview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.creditsesameringview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

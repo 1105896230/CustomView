2a0ed756c0f6ae158d9c1a8185fd9bad3f252bb2
fmt <1105896230@qq.com>
2016-09-05 14:32:25 +0800

    支付宝转盘

--- creditsesameringview/src/main/java/com/example/creditsesameringview/CreditSesameRingView.java
+++ creditsesameringview/src/main/java/com/example/creditsesameringview/CreditSesameRingView.java
@@ -30,11 +30,26 @@ package com.example.creditsesameringview;
 //                  别人笑我忒疯癫，我笑自己命太贱；
 //                  不见满街漂亮妹，哪个归得程序员？
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by "林其望".
  * DATE: 2016:09:05:14:28
  * email:1105896230@qq.com
  */
 
-public class CreditSesameRingView {
+public class CreditSesameRingView extends View {
+    public CreditSesameRingView(Context context) {
+        this(context, null);
+    }
+
+    public CreditSesameRingView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CreditSesameRingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
